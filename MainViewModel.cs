811504368910344dc5dd59ada37771bcad022bc5
Arty <belovaa@mer.ci.nsu.ru>
2024-04-14 20:59:26 +0700

    Add project files.

---
+++ MainViewModel.cs
@@ -0,0 +1,158 @@
+﻿using System.ComponentModel;
+using System.Windows.Input;
+using Microsoft.Win32;
+using System.Collections.Generic;
+
+namespace BelovTextHandlerApp
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        // public TextHandler textHandler = new TextHandler();
+        // proper mvvm ?
+
+        private string _inputFilePath;
+        private string _outputFilePath;
+        private int _minWordLength;
+        private bool _removePunctuation;
+
+        private RelayCommand _processFilesCommand;
+
+        private List<string> _selectedFromFiles = new List<string>();
+        public List<string> SelectedFromFiles
+        {
+            get { return _selectedFromFiles; }
+            set
+            {
+                _selectedFromFiles = value;
+                OnPropertyChanged(nameof(SelectedFromFiles));
+            }
+        }
+
+        private List<string> _selectedToFiles = new List<string>();
+        public List<string> SelectedToFiles
+        {
+            get { return _selectedToFiles; }
+            set
+            {
+                _selectedToFiles = value;
+                OnPropertyChanged(nameof(SelectedToFiles));
+            }
+        }
+
+        
+        private int selectedFilesCounter = 0;
+        private string _selectedFilesCount = "Выбрано файлов: 0";
+        public string SelectedFilesCount
+        {
+            get { return _selectedFilesCount; }
+            set
+            {
+                _selectedFilesCount = value;
+                OnPropertyChanged(nameof(SelectedFilesCount));
+            }
+        }
+
+        public string InputFilePath
+        {
+            get { return _inputFilePath; }
+            set
+            {
+                _inputFilePath = value;
+                OnPropertyChanged(nameof(InputFilePath));
+            }
+        }
+
+        public string OutputFilePath
+        {
+            get { return _outputFilePath; }
+            set
+            {
+                _outputFilePath = value;
+                OnPropertyChanged(nameof(OutputFilePath));
+            }
+        }
+
+        public int MinWordLength
+        {
+            get { return _minWordLength; }
+            set
+            {
+                _minWordLength = value;
+                OnPropertyChanged(nameof(MinWordLength));
+            }
+        }
+
+        public bool RemovePunctuation
+        {
+            get { return _removePunctuation; }
+            set
+            {
+                _removePunctuation = value;
+                OnPropertyChanged(nameof(RemovePunctuation));
+            }
+        }
+
+
+        public ICommand ProcessFilesCommand => _processFilesCommand ?? (_processFilesCommand = new RelayCommand(ProcessFiles));
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void ProcessFiles()
+        {
+            TextHandler textHandler = new TextHandler();
+            textHandler.ProcessFile(InputFilePath, OutputFilePath, MinWordLength, RemovePunctuation);
+        }
+
+
+        // 1 command?
+        public ICommand AddFromFilesCommand => new RelayCommand(AddFromFiles);
+        public ICommand AddToFilesCommand => new RelayCommand(AddToFiles);
+
+        private void AddFromFiles()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog.Multiselect = true;
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                SelectedFromFiles.AddRange(openFileDialog.FileNames);
+                RecountFilesNumber();
+                OnPropertyChanged(nameof(SelectedFromFiles));
+            }
+        }
+
+        private void AddToFiles()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog.Multiselect = true;
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                SelectedToFiles.AddRange(openFileDialog.FileNames);
+                RecountFilesNumber();
+                OnPropertyChanged(nameof(SelectedToFiles));
+            }
+        }
+
+        private void RecountFilesNumber()
+        {
+            if (_selectedFromFiles.Count > _selectedToFiles.Count)
+            {
+                selectedFilesCounter = _selectedFromFiles.Count;
+            }
+            else
+            {
+                selectedFilesCounter = _selectedToFiles.Count;
+            }
+            // SelectedFilesCount = $"Файлов выбрано: {0}", selectedFilesCounter;
+            SelectedFilesCount = "Файлов выбрано: " + selectedFilesCounter;
+            OnPropertyChanged(nameof(SelectedToFiles));
+        }
+    }
+}
---
+++ TextHandler.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+
+namespace BelovTextHandlerApp
+{
+    public class TextHandler
+    {
+        public void ProcessFile(string inputFilePath, string outputFilePath, int minWordLength, bool removePunctuation)
+        {
+            try
+            {
+                // Чтение текста из входного файла
+                string text = File.ReadAllText(inputFilePath);
+
+                // Обработка текста
+                if (removePunctuation)
+                {
+                    text = RemovePunctuation(text);
+                }
+
+                if (minWordLength > 0)
+                {
+                    text = RemoveShortWords(text, minWordLength);
+                }
+
+                // Запись обработанного текста в выходной файл
+                File.WriteAllText(outputFilePath, text);
+
+                Trace.WriteLine("Файл успешно обработан.");
+            }
+            catch (Exception ex)
+            {
+                Trace.WriteLine("Ошибка обработки файла: " + ex.Message);
+            }
+        }
+
+        public void ProcessFileList(List<string> inputFilePaths, List<string> outputFilePaths, int minWordLength, bool removePunctuation)
+        {
+            for (int i = 0; (i < inputFilePaths.Count) && (i < outputFilePaths.Count); i++)
+            {
+                ProcessFile(inputFilePaths[i], outputFilePaths[i], minWordLength, removePunctuation);
+            }
+        }
+
+        private string RemovePunctuation(string text)
+        {
+            // Удаление знаков препинания с использованием регулярного выражения
+            return Regex.Replace(text, @"[\p{P}\p{S}]", "");
+        }
+
+        private string RemoveShortWords(string text, int minWordLength)
+        {
+            string[] words = text.Split(new char[] { ' ', '\n', '\r', '\t' },
+                StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Length < minWordLength)
+                {
+                    words[i] = string.Empty;
+                }
+            }
+            return string.Join(" ", words);
+        }
+    }
+}
